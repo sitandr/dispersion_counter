56c8959ee8320552fcb19bf6f7c9b0e26c5ba313
donRumata <donrumata03@gmail.com>
2020-09-27 12:47:16 +0300

    fix the world

--- dispersion/num_value.py
+++ dispersion/num_value.py
@@ -1,95 +1,140 @@
 import math
 
+# TODO: fix self staticmethod; fix add dispersion
+from typing import Union
+
+def mean_square(value1, value2):
+    return math.sqrt((float(value1) ** 2 + float(value2) ** 2) / 2)
+
+def arithmetic_mean(value1, value2):
+    return (value1 + value2) / 2
+
+
 class Num_value:
+    value : float = 0
+    dispersion : float = 0
 
-      def __init__(self, value, dispersion = 0):
-            self.value = float(value)
-            self.dispersion = float(dispersion)
-            
-      def __add__(self, other):
-            
-            return Num_value(self.value + Num_value.get_v(other),
-                             (self.dispersion**2+Num_value.get_d(other)**2)**0.5)
-      def __radd__(self, other):
-            
-            return Num_value(self.value + Num_value.get_v(other),
-                             (self.dispersion**2+Num_value.get_d(other)**2)**0.5)
-      def __sub__(self, other):
-            
-            return Num_value(self.value - Num_value.get_v(other),
-                             (self.dispersion**2+Num_value.get_d(other)**2)**0.5)
-      def __rsub__(self, other):
-            
-            return Num_value( Num_value.get_v(other) - self.value,
-                             (self.dispersion**2+Num_value.get_d(other)**2)**0.5)
-      def __mul__(self, other):
-            v = self.value*Num_value.get_v(other)
-            return Num_value(v,
-                             v*(self.w()+Num_value.get_w(other)))
-      def __rmul__(self, other):
-            v = self.value*Num_value.get_v(other)
-            return Num_value(v,
-                             v*(self.w()+Num_value.get_w(other)))
-      def __truediv__(self, other):
-            v = self.value/Num_value.get_v(other)
-            return Num_value(v,
-                             v*(self.w()+Num_value.get_w(other)))
-      def __rtruediv__(self, other):
-            v = Num_value.get_v(other)/self.value
-            return Num_value(v,
-                             v*(self.w()+Num_value.get_w(other)))
-      def __pow__(self, other):
-            v = self.value**Num_value.get_v(other)
-#            return Num_value(v,
-#                             v*Num_value.get_v(other)*self.w())
-            return Num_value(v,
-                             Num_value.get_v(other)*
-                             (self.value**(Num_value.get_v(other)-1))
-                             *self.dispersion)
-      def __str__(self):
-            return '('+str(self.value)+'±'+str(self.dispersion)+')'
-      def __repr__(self):
-            return str(self)
-      
-      def __sin__(self):
-            return Num_value(math.sin(self.value),
-                             math.cos(self.value)*self.dispersion)
-      def __cos__(self):
-            return Num_value(math.cos(self.value),
-                             math.sin(self.value)*self.dispersion)
-      def __tg__(self):
-            return Num_value(math.tan(self.value),
-                             self.dispersion/math.cos(self.v)**2)
-      def __ctg__(self):
-            return Num_value(1/math.tan(self.value),
-                             self.dispersion/math.sin(self.v)**2)
-      def __ln__(self):
-            return Num_value(math.log(self.value),
-                             self.w())
-      def __log__(self, a):
-            return self.__ln__()/math.log(a)
+    def __init__(self, value, dispersion : Union[int, float] = 0.):
+        assert dispersion >= 0
 
-      def __arctg__(self):
-            return Num_value(math.atan(self.value),
-                             self.dispersion/(1+self.value))
-      
-      def get_v(obj):
-            if type(obj) == Num_value:
-                  return obj.value
-            if type(obj) in [float, int]:
-                  return float(obj)
-      def get_d(obj):
-            if type(obj) == Num_value:
-                  return obj.dispersion
-            if type(obj) in [float, int]:
-                  return 0
-      def get_w(obj):
-            if type(obj) == Num_value:
-                  return obj.w()
-            if type(obj) in [float, int]:
-                  return 0
-      def w(self):
-            if self.value !=0 :
-                  return self.dispersion/self.value
-            else:
-                  return 1
+        self.value = float(value)
+        self.dispersion = float(dispersion)
+
+    def relative_dispersion(self):
+        assert self.dispersion >= 0
+
+        if self.value != 0:
+            return self.dispersion / abs(self.value)
+        else:
+            return 1
+
+    def __add__(self, other):
+        return Num_value(
+            self.value + get_value(other), self.dispersion + get_dispersion(other)
+        )
+
+        # Num_value(self.value + get_value(other), (self.dispersion ** 2 + get_dispersion(other) ** 2) ** 0.5)
+
+    def __radd__(self, other):
+        return self.__add__(other)
+        # return Num_value(self.value + get_value(other), (self.dispersion ** 2 + get_dispersion(other) ** 2) ** 0.5)
+
+    def __sub__(self, other):
+        return Num_value(
+            self.value - get_value(other), self.dispersion + get_dispersion(other)
+        )
+
+        # Num_value(self.value - get_value(other), (self.dispersion ** 2 + get_dispersion(other) ** 2) ** 0.5)
+
+    def __rsub__(self, other):
+        self.__sub__(other)
+        # return Num_value(get_value(other) - self.value, (self.dispersion ** 2 + get_dispersion(other) ** 2) ** 0.5)
+
+    def __mul__(self, other):
+        res_value = self.value * get_value(other)
+        rel_dispersion = self.relative_dispersion() + get_relative_dispersion(other)
+
+        return Num_value(res_value, res_value * rel_dispersion)
+
+    def __rmul__(self, other):
+        return self.__mul__(other)
+        # v = self.value * Num_value.get_value(other)
+        # return Num_value(v,
+        #                  v * (self.relative_dispersion() + Num_value.get_relative_dispersion))
+
+    def __truediv__(self, other):
+        res_value = self.value / get_value(other)
+        rel_dispersion = self.relative_dispersion() + get_relative_dispersion(other)
+
+        return Num_value(res_value, res_value * rel_dispersion)
+
+    def __rtruediv__(self, other):
+        return self.__truediv__(other)
+        # v = Num_value.get_value(other) / self.value
+        # return Num_value(v,
+        #                  v * (self.relative_dispersion() + Num_value.get_relative_dispersion))
+
+    def __pow__(self, other):
+        res_value = self.value ** get_value(other)
+        #            return Num_value(res_value,
+        #                             res_value*Num_value.get_v(other)*self.w())
+        return Num_value(res_value,
+                         get_value(other) * (self.value ** (get_value(other) - 1)) * self.dispersion)
+
+
+    def __sin__(self):
+        return Num_value(math.sin(self.value),
+                         math.cos(self.value) * self.dispersion)
+
+    def __cos__(self):
+        return Num_value(math.cos(self.value),
+                         math.sin(self.value) * self.dispersion)
+
+    def __tg__(self):
+        return Num_value(math.tan(self.value),
+                         self.dispersion / math.cos(self.value) ** 2)
+
+    def __ctg__(self):
+        return Num_value(1 / math.tan(self.value),
+                         self.dispersion / math.sin(self.value) ** 2)
+
+    def __ln__(self):
+        return Num_value(math.log(self.value),
+                         self.relative_dispersion())
+
+    def __log__(self, a : Union[float, int]):
+        return self.__ln__() / math.log(float(a))
+
+    def __arctg__(self):
+        return Num_value(math.atan(self.value),
+                         self.dispersion / (1 + self.value))
+    # Conversion to string
+    def __str__(self):
+        return '(' + str(self.value) + '±' + str(self.dispersion) + ')'
+
+    def __repr__(self):
+        return str(self)
+
+
+
+def get_value(obj: Union[Num_value, float, int]):
+    if type(obj) == Num_value:
+        return obj.value
+    if type(obj) in [float, int]:
+        return float(obj)
+    else:
+        raise TypeError
+
+
+def get_dispersion(obj : Union[Num_value, float, int]):
+    if type(obj) == Num_value:
+        return obj.dispersion
+    if type(obj) in [float, int]:
+        return 0
+
+
+def get_relative_dispersion(obj : Union[Num_value, float, int]):
+    if type(obj) == Num_value:
+        return obj.relative_dispersion()
+    if type(obj) in [float, int]:
+        return 0
--- test.py
+++ test.py
@@ -1,2 +1,17 @@
-import dispersion.dispersion as d
-d.console_mode()
+from dispersion.dispersion import *
+
+
+# d.console_mode()
+
+
+
+# i : int = 1
+# print(type(i))
+# print(type(i) == int)
+# print(type(i) in [int, float])
+#
+# a = 1
+# b = 10
+#
+# print(d.arithmetic_mean(a, b), d.mean_square(a, b))
+#
