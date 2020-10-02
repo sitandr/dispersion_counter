97d2457d0a03fefb125ef35469bc0488b7ac177e
Andrew <andr-sitnikov@mail.ru>
2020-10-02 21:15:50 +0300

    Added error of incorrect input of dispersed value

--- test.py
+++ test.py
@@ -1,17 +1,4 @@
 from dispersion.dispersion import *
 
 
-# d.console_mode()
-
-
-
-# i : int = 1
-# print(type(i))
-# print(type(i) == int)
-# print(type(i) in [int, float])
-#
-# a = 1
-# b = 10
-#
-# print(d.arithmetic_mean(a, b), d.mean_square(a, b))
-#
+console_mode()
